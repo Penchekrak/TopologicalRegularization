6269c5be6a480fe9f4666ea7ea60700bf202beed
penchekrak <spiand97@gmail.com>
2023-03-03 10:20:40 +0400

    remove host-specificscripts

--- run_train_beleriand.sh
+++
@@ -1,9 +0,0 @@
-source ~/mambaforge/etc/profile.d/conda.sh
-conda activate topo
-export TORCH_EXTENSIONS_DIR=~/torch_extensions/
-export CUDA_HOME=~/mambaforge/envs/topo/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/mambaforge/envs/topo/lib
-export LIBRARY_PATH=$LIBRARY_PATH:~/mambaforge/envs/topo/lib/
-export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export HYDRA_FULL_ERROR=1
-python train.py "$@"
